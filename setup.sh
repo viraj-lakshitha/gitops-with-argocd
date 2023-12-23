d02357d6eaba191cad224819bec154583b66d845
Viraj Lakshitha <virajlakshithabandara@gmail.com>
2023-12-23 12:39:40 +0530

    feat: deploy new application version1.1

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # Apply initial application configurations
-kubectl apply -f application.yaml
+# kubectl apply -f application.yaml
